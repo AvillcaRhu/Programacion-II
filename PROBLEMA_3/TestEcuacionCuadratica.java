b1c5ab62966fb0c5e9b76b312d797c5d7e7e5df5
AvillcaRhu <avillcarhu@gmail.com>
2026-09-02 01:41:59 -0400

    Add files via upload

---
+++ PROBLEMA_3/TestEcuacionCuadratica.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class TestEcuacionCuadratica {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Ingrese a, b, c: ");
+    double a = sc.nextDouble();
+    double b = sc.nextDouble();
+    double c = sc.nextDouble();
+    EcuacionCuadratica ecuacion = new EcuacionCuadratica(a, b, c);
+    double disc = ecuacion.getDiscriminante();
+    if (disc > 0) {
+      System.out.println("La ecuacion tiene dos raices " + ecuacion.getRaiz1()
+          + " y " + ecuacion.getRaiz2());
+    } else if (disc == 0) {
+      System.out.println("La ecuacion tiene una raiz " + ecuacion.getRaiz1());
+    } else {
+      System.out.println("La ecuacion no tiene raices reales");
+    }
+    sc.close();
+  }
+}
